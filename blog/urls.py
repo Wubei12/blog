8b9b05ca00c0e33172c82fac8a4a63fad42dbba5
Wubshet Demissie <wubshetyenguss@gmail.com>
2022-06-26 23:41:41 +0300

    initial commit

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from unicodedata import name
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.BlogListView.as_view(), name='home'),
+    path('post/<int:pk>/', views.BlogDetailView.as_view(), name='post_detail'),
+]
